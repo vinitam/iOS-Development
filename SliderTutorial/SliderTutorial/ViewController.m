a88b90781cb181fdaaf0ff1b6c90bfba03010ca0
Vinita Miranda <vinita.vins2007@gmail.com>
2015-01-26 12:26:33 +0530

    Slider tutorial added.

---
+++ SliderTutorial/SliderTutorial/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  SliderTutorial
+//
+//  Created by Vinita Miranda on 1/26/15.
+//  Copyright (c) 2015 Vinita Miranda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)sliderChanged:(id)sender
+{
+    NSLog(@"SliderValue %f",self.slider.value);
+}
+
+@end
