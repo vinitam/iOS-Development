01bd25bddcc758431f96aa956a2f4e30b926cb75
Vinita Miranda <vinita.vins2007@gmail.com>
2015-01-26 12:23:42 +0530

    Switch tutorial added.

---
+++ SwitchTutorial/SwitchTutorial/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  SwitchTutorial
+//
+//  Created by Vinita Miranda on 1/26/15.
+//  Copyright (c) 2015 Vinita Miranda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+-(IBAction)switched:(id)sender
+{
+    NSLog(@"Switch current state %@", self.switchControl.on ? @"On" : @"Off");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
