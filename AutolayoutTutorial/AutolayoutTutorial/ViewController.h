a89e524cd92eb94ea644a537d87cf84287ce8fad
Vinita Miranda <vinita.vins2007@gmail.com>
2015-02-05 15:06:27 +0530

    Example of constraints added.

---
+++ AutolayoutTutorial/AutolayoutTutorial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutolayoutTutorial
+//
+//  Created by Vinita Miranda on 2/5/15.
+//  Copyright (c) 2015 Vinita Miranda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
