c42f154acf7458c0d5d16293b3d1ed954d785e1c
Vinita Miranda <vinita.vins2007@gmail.com>
2015-02-20 20:44:40 +0530

    Messaging added

--- EmailDemo/EmailDemo/ViewController.m
+++ EmailDemo/EmailDemo/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #import <MessageUI/MessageUI.h>
 
-@interface ViewController () <MFMailComposeViewControllerDelegate>
+@interface ViewController () <MFMailComposeViewControllerDelegate,MFMessageComposeViewControllerDelegate>
 @property(nonatomic)IBOutlet UILabel *emailLabel;
 -(IBAction)buttonEmailClicked:(id)sender;
 @end
@@ -59,10 +59,54 @@
     }
 }
 
+-(IBAction)buttonMessageClicked:(id)sender
+{
+    if(![MFMessageComposeViewController canSendText]) {
+        UIAlertView *warningAlert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Your device doesn't support SMS!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [warningAlert show];
+        return;
+    }
+    
+    NSArray *recipents = @[@"0000000000"];
+    NSString *message = @"Good morning";
+    
+    MFMessageComposeViewController *messageController = [[MFMessageComposeViewController alloc] init];
+    messageController.messageComposeDelegate = self;
+    [messageController setRecipients:recipents];
+    [messageController setBody:message];
+    
+    // Present message view controller on screen
+    [self presentViewController:messageController animated:YES completion:nil];
+
+}
+
 - (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
 {
     [controller dismissViewControllerAnimated:YES completion:nil];
 }
 
+
+- (void)messageComposeViewController:(MFMessageComposeViewController *)controller didFinishWithResult:(MessageComposeResult) result
+{
+    switch (result) {
+        case MessageComposeResultCancelled:
+            break;
+            
+        case MessageComposeResultFailed:
+        {
+            UIAlertView *warningAlert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Failed to send SMS!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [warningAlert show];
+            break;
+        }
+            
+        case MessageComposeResultSent:
+            break;
+            
+        default:
+            break;
+    }
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
